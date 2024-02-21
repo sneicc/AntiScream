888f496fc4cc6275d211bc72e5d088d599918bfd
Andrey <102552777+sneicc@users.noreply.github.com>
2024-02-21 23:12:44 +0300

    Change git folder

---
+++ AntiScreamConfigurator/Form1.cs
@@ -0,0 +1,10 @@
+namespace AntiScreamF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AntiScreamSaveManager/SaveManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json;
+
+namespace AntiScreamSaveManager
+{
+    public static class SaveManager
+    {
+        public static void Save(DataHolder dataHolder)
+        {
+
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AntiScreamSave.json");
+
+            try
+            {
+                string storedData = JsonSerializer.Serialize(dataHolder);
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.Write(storedData);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public static DataHolder Load()
+        {
+            string fullPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AntiScreamSave.json");
+
+            DataHolder? loadedData = null;
+            if (File.Exists(fullPath))
+            {
+                try
+                {
+                    string dataToLoad = string.Empty;
+                    using (FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(fs))
+                        {
+                            dataToLoad = sr.ReadToEnd();
+                        }
+                    }
+
+                    loadedData = JsonSerializer.Deserialize<DataHolder>(dataToLoad);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+            return loadedData;
+        }
+    }
+
+    public class DataHolder
+    {
+        public double ScreamThreshold { get; set; }
+        public int ScreamTimeout { get; set; }
+        public string AlertMessage { get; set; }
+        public int MicID { get; set; }
+        public List<string>? ProgramsToKill { get; set; }
+
+        public DataHolder()
+        {
+            ScreamThreshold = 0;
+            ScreamTimeout = 0;
+            AlertMessage = "";
+            MicID = 0;
+            ProgramsToKill = null;
+        }
+    }
+}
